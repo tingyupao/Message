1256ea9cc26eecd57b3d72eeb3ecaa0ecf4734e3
Tingyu Pao <paotingyu@gmail.com>
2017-08-16 21:01:26 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    @RequestMapping("/")
+    public String listMessages(Model model){
+        model.addAttribute("messages", messageRepository.findAll());
+        return "messagelist";
+    }
+
+    @RequestMapping("/addmessage")
+    public String messageForm(Model model){
+        model.addAttribute("message", new Message());
+        return "messageform";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Message message, BindingResult result){
+        if(result.hasErrors()){
+            return "messageform";
+        }
+        messageRepository.save(message);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showJob(@PathVariable("id")long id, Model model){
+        model.addAttribute("message", messageRepository.findOne(id));
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateMessage(@PathVariable("id") long id, Model model){
+        model.addAttribute("message", messageRepository.findOne(id));
+        return "messageform";
+    }
+}
---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Message {
+
+    @Id@GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=10)
+    private String content;
+
+    @NotNull
+    @Size(max=10)
+    private String date;
+
+    @NotNull
+    @Size(min=2)
+    private String sender;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
